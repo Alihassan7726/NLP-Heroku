1f7832bdf868adb50b8004b7de62a8690d91fbac
Alihassan7726 <75217939+Alihassan7726@users.noreply.github.com>
2020-12-06 14:07:58 +0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,6 +13,11 @@ from sklearn.naive_bayes import MultinomialNB
 import joblib
 
 app = Flask(__name__)
+NB_spam_model = open('NB_spam_model.pkl','rb')
+    clf = joblib.load(NB_spam_model)
+    cv_model = open('cv.pkl', 'rb')
+    cv = joblib.load(cv_model)
+    
 
 @app.route('/')
 def home():
@@ -23,7 +28,6 @@ def predict_fun():
 	
     NB_spam_model = open('NB_spam_model.pkl','rb')
     clf = joblib.load(NB_spam_model)
-    
     cv_model = open('cv.pkl', 'rb')
     cv = joblib.load(cv_model)
     
