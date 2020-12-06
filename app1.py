28f378067a3f0a78181ed0cd940a6eebaac3c0bd
Alihassan7726 <75217939+Alihassan7726@users.noreply.github.com>
2020-12-06 14:11:08 +0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -14,9 +14,9 @@ import joblib
 
 app = Flask(__name__)
 NB_spam_model = open('NB_spam_model.pkl','rb')
-    clf = joblib.load(NB_spam_model)
-    cv_model = open('cv.pkl', 'rb')
-    cv = joblib.load(cv_model)
+clf = joblib.load(NB_spam_model)
+cv_model = open('cv.pkl', 'rb')
+cv = joblib.load(cv_model)
     
 
 @app.route('/')
@@ -25,19 +25,12 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict_fun():
-	
-    NB_spam_model = open('NB_spam_model.pkl','rb')
-    clf = joblib.load(NB_spam_model)
-    cv_model = open('cv.pkl', 'rb')
-    cv = joblib.load(cv_model)
-    
-    if request.method == 'POST':
-        message = request.form['message']
-        data = [message]
-        vect = cv.transform(data).toarray()
-        my_prediction = clf.predict(vect)
-	
-    return render_template('result.html',prediction = my_prediction)
+	if request.method == 'POST':
+		message = request.form['message']
+                data = [message]
+                vect = cv.transform(data).toarray()
+                my_prediction = clf.predict(vect)
+	return render_template('result.html',prediction = my_prediction)
 
 
 if __name__ == '__main__':
